dccf21001fed2907e4e3742ca21ddc7f28a036d3
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2025-12-21 12:45:19 -0500

    finish part2

--- aoc-2025/src/day1.ts
+++ aoc-2025/src/day1.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 
 async function solvePartOne(fp: string, curr: number): Promise<number> {
-    const f = await open(fp)
+    const f = await open(fp);
     let res = 0;
 
     for await (const r of f.readLines()) {
@@ -20,15 +20,54 @@ async function solvePartOne(fp: string, curr: number): Promise<number> {
         }
     }
 
+    return res;
+}
+
+
+async function solvePartTwo(fp: string, curr: number): Promise<number> {
+    const f = await open(fp);
+    let res = 0;
+
+    const calcRedundants = (rotation: number) => {
+        return Math.floor(rotation / 100);
+    };
+
+    for await (const r of f.readLines()) {
+        const direction = r[0];
+        let rotation = Number(r.replace(r[0], ""));
+        const redundants = calcRedundants(rotation);
+
+        res = res + redundants;
+        rotation = rotation - (redundants * 100);
+
+        if (direction == "L") {
+            const prev = curr;
+            const temp = curr - rotation;
+            curr = (temp + 100) % 100;
+
+            if (prev > 0 && temp <= 0) 
+                res = res + 1;
+        }
+        else {
+            const prev = curr;
+            const temp = curr + rotation;
+            curr = temp % 100;
+
+            if (prev < 100 && temp >= 100)
+                res = res + 1;
+        }
+
+    }
+
     return res
 }
 
 
 async function unlockSafe(fp: string, curr: number): Promise<[number, number]> {
     const partOneRes = await solvePartOne(fp, curr);
-    // const partTwoRes = await solvePartTwo();
+    const partTwoRes = await solvePartTwo(fp, curr);
 
-    return [partOneRes, 0];
+    return [partOneRes, partTwoRes];
 }
 
 
