50fb8991a14c2870246a4a652c87deea9bf5bf06
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2025-12-25 00:56:20 -0500

    working with test data

--- aoc-2025/src/day2.ts
+++ aoc-2025/src/day2.ts
@@ -31,19 +31,38 @@ function parseContents(contents: string): Array<Range> {
 
 
 function countInvalidIds(ranges: Array<Range>): number {
+    let total = 0;
+
     for (const r of ranges) {
-        console.log(r);
+        console.log("\nDoing range: " + r.lower + " - " + r.upper);
+
+        for (let i=r.lower; i<=r.upper; i++) {
+            console.log(" > " + i);
+            const i_str = i.toString();
+            const length = i_str.length;
+
+            if (length % 2 == 0) {
+                const left = i_str.substring(0, length/2);
+                const right = i_str.substring(length/2);
+                console.log("   - left=" + left + ", right=" + right);
+
+                if (left == right) {
+                    console.log("------------------ADDED");
+                    total = total + i;
+                }
+            }
+        }
     }
 
-    return 0;
+    return total;
 }
 
 
 async function main() {
     const filePath = path.join(__dirname, "../assets/day2.txt");
-    const contents = await readInput(filePath);
+    let contents = await readInput(filePath);
+    contents = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
     const ranges = parseContents(contents);
-
     const res = countInvalidIds(ranges);
 
     console.log(res)
