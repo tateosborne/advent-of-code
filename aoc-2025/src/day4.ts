e6bbad491d0c25ab0937db8277c674f626d6349d
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2026-01-24 00:49:46 -0500

    parse contents

--- aoc-2025/src/day4.ts
+++ aoc-2025/src/day4.ts
@@ -1,11 +1,21 @@
 import path from "path";
 import { read } from "./lib/readFile";
 
+function parseToArray(str: string): string[][] {
+  const temp: string[] = str.split("\n");
+  const arr: string[][] = [];
+
+  for (const row of temp) arr.push(row.split(""));
+
+  return arr;
+}
 
 async function main() {
   const filePath = path.join(__dirname, "../assets/day4/sample.txt");
   const contents = await read(filePath);
-  console.log(contents);
+
+  const res = parseToArray(contents);
+  console.log(res);
 }
 
 main();
