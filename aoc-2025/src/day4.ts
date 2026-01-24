e7421dce597bd6c126307fa221981c49c863cad2
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2026-01-24 01:05:44 -0500

    rough plan mapped out

--- aoc-2025/src/day4.ts
+++ aoc-2025/src/day4.ts
@@ -1,20 +1,41 @@
 import path from "path";
 import { read } from "./lib/readFile";
 
-function parseToArray(str: string): string[][] {
+interface Coordinate {
+    x: number;
+    y: number;
+};
+
+function parseGrid(str: string): string[][] {
   const temp: string[] = str.split("\n");
-  const arr: string[][] = [];
+  const grid: string[][] = [];
 
-  for (const row of temp) arr.push(row.split(""));
+  for (const row of temp) grid.push(row.split(""));
 
-  return arr;
+  return grid;
+}
+
+function findAccessibleRolls(grid: string[][]): number {
+    /**
+     * for each element (iterate horizontally)
+     * check bordering tiles 
+     * early 'continue' if count == 4 
+     * if < 4, update running total
+     */
+    let total = 0;
+
+    const bfs = (curr: Coordinate) => {curr};
+
+    return total;
 }
 
 async function main() {
   const filePath = path.join(__dirname, "../assets/day4/sample.txt");
   const contents = await read(filePath);
 
-  const res = parseToArray(contents);
+  const grid = parseGrid(contents);
+  const res = findAccessibleRolls(grid);
+
   console.log(res);
 }
 
